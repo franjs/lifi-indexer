7dc0a1c15092112d0384c43b6b35f922a56acdd2
Francisco Silva <franjs.francisco@gmail.com>
2024-01-28 18:54:27 +0100

    updating feesCollectedService

--- src/services/feesCollectedService.ts
+++ src/services/feesCollectedService.ts
@@ -8,3 +8,23 @@ export async function saveFeesCollected(
 ): Promise<void> {
   await FeesCollectedModel.insertMany(feesCollected);
 }
+
+export async function getFeesCollected(
+  filter?: Partial<FeesCollected>,
+  limit = 25,
+  offset = 0,
+): Promise<FeesCollected[]> {
+  return FeesCollectedModel.find({ ...filter })
+    .limit(limit)
+    .skip(offset)
+    .select("-__v")
+    .lean();
+}
+
+export async function getFeesCollectedCount(
+  filter?: Partial<FeesCollected>,
+): Promise<number> {
+  return FeesCollectedModel.countDocuments({
+    ...filter,
+  });
+}
